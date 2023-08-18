83713fa3b4a27e8719a23cef6e21cb40954ed5d3
mas-python <ali.shehper1@gmail.com>
2023-08-18 11:24:32 -0400

    added tensorboard.sh

---
+++ shell_files/tensorboard.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --job-name="tensorboard"
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+
+source .env/bin/activate
+
+echo "Starting ${logdir} on port ${port}."
+
+tensorboard --logdir=$logdir 
