11c5e97066057d8f528f7192880e3c833619d8e6
mas-python <ali.shehper1@gmail.com>
2023-08-21 20:19:12 -0400

    added fixed node to atari_cleanrl.sh

--- shell_files/atari_cleanrl.sh
+++ shell_files/atari_cleanrl.sh
@@ -9,8 +9,7 @@
 #SBATCH --output=slurm.%j.out  # send stdout to outfile
 #SBATCH --error=slurm.%j.err  # send stderr to errfile
 #SBATCH -t 24:00:00  # time requested in hour:minute:second
-
-# Number of gpus per node is specified as #SBATCH --gpus-per-node=2
+#SBATCH --nodelist=hal0168
 
 source ./env/bin/activate
 python src/ppo_atari.py --track=True
