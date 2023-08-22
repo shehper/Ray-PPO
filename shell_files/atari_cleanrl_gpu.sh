1d1ec4fd4644af0cd12728625d83c08ca6ce3eab
mas-python <ali.shehper1@gmail.com>
2023-08-21 21:34:50 -0400

    excluded bad gpu nodes from atari_cleanrl_gpu.sh

--- shell_files/atari_cleanrl_gpu.sh
+++ shell_files/atari_cleanrl_gpu.sh
@@ -6,10 +6,11 @@
 #SBATCH --ntasks=1                # Total # of tasks across all nodes
 #SBATCH --cpus-per-task=8        # Cores per task (>1 if multithread tasks)
 #SBATCH --mem=3000                # Real memory (RAM) required (MB)
-#SBATCH --time=01:00:00           # Total run time limit (HH:MM:SS)
+#SBATCH --time=10:00:00           # Total run time limit (HH:MM:SS)
 #SBATCH --output=./slurm.gpu.%N.%j.out
 #SBATCH --error=./slurm.gpu.%N.%j.err
 #SBATCH --gres=gpu:1
+#SBATCH --exclude=cuda[001-008],gpu[005-008],pascal[001-010],volta[001-003]  # blacklist slow nodes
 
 source ./env/bin/activate
 python src/ppo_atari.py --track=True
