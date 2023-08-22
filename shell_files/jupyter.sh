023d7f009fff1e90188dae1a0743889fe4fde95c
mas-python <ali.shehper1@gmail.com>
2023-08-21 21:42:45 -0400

    final versions of jupyter and tensorboard shell files hopefully

--- shell_files/jupyter.sh
+++ shell_files/jupyter.sh
@@ -1,27 +1,19 @@
 #!/bin/bash
-
 #SBATCH --partition=main             # Partition (job queue)
-
 #SBATCH --job-name=jupyter          # Assign an short name to your job
-
 #SBATCH --nodes=1                    # Number of nodes you require
-
 #SBATCH --ntasks=1                   # Total # of tasks across all nodes
-
 #SBATCH --cpus-per-task=2            # Cores per task (>1 if multithread tasks)
-
 #SBATCH --mem=4000                 # Real memory (RAM) required (MB)
-
 #SBATCH --time=02:00:00              # Total run time limit (HH:MM:SS)
-
 #SBATCH --output=slurm.%N.%j.out     # STDOUT output file
-
 #SBATCH --error=slurm.%N.%j.err      # STDERR output file (optional)
 
+module load python/3.9.6-gc563
 mkdir /scratch/$USER/XDGRUNTIMEDIR
-
 export XDGRUNTIMEDIR=/scratch/$USER/XDGRUNTIMEDIR
-
 export XDGRUNTIMEDIR=$HOME/tmp   ## needed for jupyter writting temporary files
 
 srun jupyter notebook --no-browser --ip=0.0.0.0 --port=8889
+
+module unload python/3.9.6-gc563
--- shell_files/tensorboard.sh
+++ shell_files/tensorboard.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-#SBATCH --partition=main             # Partition (job queue)
-#SBATCH --job-name=tensorboard          # Assign an short name to your job
 #SBATCH --nodes=1                    # Number of nodes you require
 #SBATCH --ntasks=1                   # Total # of tasks across all nodes
 #SBATCH --cpus-per-task=2            # Cores per task (>1 if multithread tasks)
@@ -9,15 +7,10 @@
 #SBATCH --output=slurm.%N.%j.out     # STDOUT output file
 #SBATCH --error=slurm.%N.%j.err      # STDERR output file (optional)
 
-# mkdir /scratch/$USER/XDGRUNTIMEDIR
-# export XDGRUNTIMEDIR=/scratch/$USER/XDGRUNTIMEDIR
-# export XDGRUNTIMEDIR=$HOME/tmp   ## needed for jupyter writting temporary files
-# srun jupyter notebook --no-browser --ip=0.0.0.0 --port=8889
+module load python/3.9.6-gc563
+mkdir /scratch/$USER/XDGRUNTIMEDIR
+export XDGRUNTIMEDIR=/scratch/$USER/XDGRUNTIMEDIR
+export XDGRUNTIMEDIR=$HOME/tmp   ## needed for jupyter writting temporary files
+srun tensorboard --logdir=/scratch/mas1107/rlalgo --host=0.0.0.0 --port=8887
 
-source ./env/bin/activate
-
-# echo "Starting ${logdir} on port ${port}."
-
-# tensorboard --logdir=/scratch/mas1107/rlalgo --port=16006
-
-srun tensorboard --logdir=/scratch/mas1107/rlalgo --host=0.0.0.0 --port=8889
+module unload python/3.9.6-gc563
