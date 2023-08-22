e455cc55c2afb7b13292f8f5f857f475f8a7e9e1
mas-python <ali.shehper1@gmail.com>
2023-08-21 20:34:17 -0400

    changes to tensorboard.sh

--- shell_files/tensorboard.sh
+++ shell_files/tensorboard.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
-#SBATCH --job-name="tensorboard"
-#SBATCH --nodes=1
-#SBATCH --ntasks=1
-#SBATCH --cpus-per-task=1
+#SBATCH --partition=main             # Partition (job queue)
+#SBATCH --job-name=tensorboard          # Assign an short name to your job
+#SBATCH --nodes=1                    # Number of nodes you require
+#SBATCH --ntasks=1                   # Total # of tasks across all nodes
+#SBATCH --cpus-per-task=2            # Cores per task (>1 if multithread tasks)
+#SBATCH --mem=4000                 # Real memory (RAM) required (MB)
+#SBATCH --time=02:00:00              # Total run time limit (HH:MM:SS)
+#SBATCH --output=slurm.%N.%j.out     # STDOUT output file
+#SBATCH --error=slurm.%N.%j.err      # STDERR output file (optional)
+
+# mkdir /scratch/$USER/XDGRUNTIMEDIR
+# export XDGRUNTIMEDIR=/scratch/$USER/XDGRUNTIMEDIR
+# export XDGRUNTIMEDIR=$HOME/tmp   ## needed for jupyter writting temporary files
+# srun jupyter notebook --no-browser --ip=0.0.0.0 --port=8889
 
 source ./env/bin/activate
 
