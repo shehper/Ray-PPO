69d0f0f43170dbe4d4552aa10f30e535fa159228
mas-python <ali.shehper1@gmail.com>
2023-08-23 20:48:03 -0400

    small changes to ppo_procgen and added a shell file for procgen

--- src/cleanrl_ppo_procgen.py
+++ src/cleanrl_ppo_procgen.py
@@ -167,8 +167,9 @@ if __name__ == "__main__":
             name=run_name,
             monitor_gym=True,
             save_code=True,
+            mode="offline"
         )
-    writer = SummaryWriter(f"runs/{run_name}")
+    writer = SummaryWriter(f"procgen-runs/{run_name}")
     writer.add_text(
         "hyperparameters",
         "|param|value|\n|-|-|\n%s" % ("\n".join([f"|{key}|{value}|" for key, value in vars(args).items()])),
