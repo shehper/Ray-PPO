89a3efd6e2d44de3ea991b1a7aed9ff1151fdb36
mas-python <ali.shehper1@gmail.com>
2023-08-23 22:18:32 -0400

    minor changes

--- shell_files/procgen_cleanrl.sh
+++ shell_files/procgen_cleanrl.sh
@@ -5,7 +5,7 @@
 #SBATCH -N 1      # nodes requested
 #SBATCH -n 1      # tasks requested
 #SBATCH -c 8      # cores requested
-#SBATCH --mem=1000  # memory in Mb
+#SBATCH --mem=128000  # memory in Mb
 #SBATCH --output=slurm.%j.out  # send stdout to outfile
 #SBATCH --error=slurm.%j.err  # send stderr to errfile
 #SBATCH -t 24:00:00  # time requested in hour:minute:second
